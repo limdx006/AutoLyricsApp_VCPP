6c28366a12ec518c5c05d21d124b628c597d56a1
LimDongXian <limdongx@gmail.com>
2026-07-17 15:56:07 +0800

    Update build and rename main cpp
    use *.cpp to include all possible cpp file so that it auto detect and build even if any new cpp file added.
    
    Rename main entrance  to avoid confusion

--- build.bat
+++ build.bat
@@ -3,11 +3,12 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64
  
 cd /d D:\Personal_GitHub_rep\AutoLyricsApp_VCPP
- 
+
 cl /EHsc /std:c++20 ^
-version_1.cpp ^
-components\media_session.cpp ^
-components\lyrics_fetcher.cpp ^
+main.cpp ^
+components\*.cpp ^
+/Fo"obj\\" ^
+/Fd"obj\\" ^
 /I"D:\Personal_GitHub_rep\AutoLyricsApp_VCPP\components" ^
 /I"D:\Personal_GitHub_rep\AutoLyricsApp_VCPP\vcpkg\installed\x64-windows\include" ^
 /link ^
