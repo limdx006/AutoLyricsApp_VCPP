1976b4b148411f9dd023c0359875e3761937f0c5
LimDongXian <limdongx@gmail.com>
2026-07-17 14:38:30 +0800

    Dump Json
    Using Json instead of print and store for future use enabling expanding the information

--- build.bat
+++ build.bat
@@ -1,6 +1,12 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64
 
-cl /EHsc /std:c++20 version_1.cpp /link windowsapp.lib
+cd /d D:\Personal_GitHub_rep\AutoLyricsApp_VCPP
+
+cl /EHsc /std:c++20 ^
+version_1.cpp ^
+/I"D:\Personal_GitHub_rep\AutoLyricsApp_VCPP\vcpkg\installed\x64-windows\include" ^
+/link ^
+windowsapp.lib
 
 pause
--- version_1.cpp
+++ version_1.cpp
@@ -9,12 +9,13 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Media.Control.h>
 
+#include <nlohmann/json.hpp> // For JSON parsing
 
 // Namespace declarations for convenience
 using namespace std;
 using namespace winrt;
 using namespace winrt::Windows::Media::Control;
-
+using json = nlohmann::json;
 
 // Function declarations
 string get_lyrics(const string& title, const string& artist);
@@ -35,12 +36,16 @@ int main() {
     if (session)
     {
         auto info = session.TryGetMediaPropertiesAsync().get();
-        string title = winrt::to_string(info.Title());
-        string artist = winrt::to_string(info.Artist());
-        cout << "Now Playing: " << title << " by " << artist << std::endl;
+        string title = to_string(info.Title());
+        string artist = to_string(info.Artist());
+        cout << "Now Playing: " << title << " by " << artist << endl;
         cout << "Fetching lyrics..." << "\n";
-        string lyrics = get_lyrics(title, artist);
-        cout << "Lyrics: \n" << lyrics;
+        string response = get_lyrics(title, artist);
+        json j = json::parse(response);
+        if (j["success"]){
+            string lyrics = j["lyrics"];
+            cout << "Lyrics:\n" << lyrics << std::endl;
+        }
     }
     else
     {
@@ -54,25 +59,31 @@ int main() {
 }
 
 
-std::string get_lyrics(const string& title,
-                       const string& artist)
+std::string get_lyrics(const string& title, const string& artist)
 {
-    string command = "python lyrics_fetcher.py \"" + title + "\" \"" + artist + "\"";
+    // Convert UTF-8 std::string -> UTF-16 for SetEnvironmentVariableW
+    auto toWide = [](const string& s) -> wstring {
+        if (s.empty()) return L"";
+        int len = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, nullptr, 0);
+        wstring w(len, 0);
+        MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, &w[0], len);
+        return w;
+    };
+
+    SetEnvironmentVariableW(L"TRACK_TITLE", toWide(title).c_str());
+    SetEnvironmentVariableW(L"TRACK_ARTIST", toWide(artist).c_str());
+
+    string command = "python lyrics_fetcher.py";
 
     array<char, 256> buffer;
     string result;
 
     FILE* pipe = _popen(command.c_str(), "r");
-
-    if (!pipe)
-        return "";
+    if (!pipe) return "";
 
     while (fgets(buffer.data(), buffer.size(), pipe))
-    {
         result += buffer.data();
-    }
 
     _pclose(pipe);
-
     return result;
 }
